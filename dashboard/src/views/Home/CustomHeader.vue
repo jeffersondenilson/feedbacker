5dda027b0b1399d4e6a3e6509019869335222639
Jefferson Denilson <jefersond121212@gmail.com>
2021-03-15 15:00:21 -0300

    home e contato

---
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>custom header</div>
+</template>
+
+<script>
+export default {
+  name: 'CustomHeader'
+}
+</script>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,42 @@
 <template>
-	<h1 class="text-brand-main">Home</h1>
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <contact />
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">
+      Feedbacker © 2021
+    </p>
+  </footer>
 </template>
 
 <script>
-	export default {
-		name: 'index'
-	}
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('feedbacker-token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {}
+
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
 </script>
