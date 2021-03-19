f737bdd4b8aa7c21cc0b07e00c5e03c0031cdfde
Jefferson Denilson <jefersond121212@gmail.com>
2021-03-19 16:23:06 -0300

    verificando acesso a rota no lado cliente

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,11 +4,28 @@
 </template>
 
 <script>
+import { watch } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import ModalFactory from './components/ModalFactory'
+import services from './services'
 
 export default {
-  components: {
-    ModalFactory
+  components: { ModalFactory },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+        // redireciona p/ Home caso o token não seja valido (401)
+        const { data } = await services.users.getMe()
+      }
+    })
   }
 }
 </script>
