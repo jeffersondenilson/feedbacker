3e86690cd0280a741793ad360df874a3e8116839
Jefferson Denilson <jefersond121212@gmail.com>
2021-04-07 14:05:23 -0300

    link temporario p/ credentials

--- dashboard/src/views/Feedbacks/index.vue
+++ dashboard/src/views/Feedbacks/index.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>Feedbacks</h1>
+  <br>
+  <a href="/credentials" @click.prevent="() => router.push({ name: 'Credentials' })">
+    credentials
+  </a>
 </template>
+
+<script>
+import { useRouter } from 'vue-router'
+
+export default {
+  setup () {
+    const router = useRouter()
+
+    return {
+      router
+    }
+  }
+}
+</script>
