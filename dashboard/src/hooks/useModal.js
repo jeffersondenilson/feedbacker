5c47723a8e0de5e9870470b36756e00c9414e240
Jefferson Denilson <jefersond121212@gmail.com>
2021-04-25 20:16:11 -0300

    testes de useModal

--- dashboard/src/hooks/useModal.js
+++ dashboard/src/hooks/useModal.js
@@ -4,11 +4,11 @@ const EVENT_NAME = 'modal:toggle'
 
 export default function useModal () {
   function open (payload = {}) {
-    bus.emit(EVENT_NAME, { status: true, ...payload })
+    bus.emit(EVENT_NAME, { ...payload, status: true })
   }
 
   function close (payload = {}) {
-    bus.emit(EVENT_NAME, { status: false, ...payload })
+    bus.emit(EVENT_NAME, { ...payload, status: false })
   }
 
   function listen (fn) {
---
+++ dashboard/src/hooks/useModal.spec.js
@@ -0,0 +1,64 @@
+import bus from '../utils/bus'
+import useModal from './useModal'
+
+const EVENT_NAME = 'modal:toggle'
+
+describe('useModal hooks', () => {
+  afterEach(() => {
+    bus.off(EVENT_NAME)
+  })
+
+  it('should listen to event', () => {
+    const modal = useModal()
+    const payload = { status: true, component: 'ModalCreateAccount' }
+    const spy = jest.fn()
+
+    modal.listen(spy)
+    bus.emit(EVENT_NAME, { ...payload })
+
+    expect(spy).toBeCalledTimes(1)
+    expect(spy).toBeCalledWith(payload)
+  })
+
+  it('should unlisten to event', () => {
+    const modal = useModal()
+    const payload = { status: true, component: 'ModalCreateAccount' }
+    const spy = jest.fn()
+    modal.listen(spy)
+
+    modal.off(spy)
+    bus.emit(EVENT_NAME, payload)
+
+    expect(spy).not.toBeCalled()
+  })
+
+  it('should emit event with payload { status: true, ...rest }', () => {
+    const modal = useModal()
+    const payload = { component: 'ModalCreateAccount' }
+    const spy = jest.fn()
+    modal.listen(spy)
+
+    modal.open({ ...payload })
+
+    expect(spy).toBeCalledTimes(1)
+    expect(spy).toBeCalledWith({
+      status: true,
+      ...payload
+    })
+  })
+
+  it('should emit event with payload { status: false, ...rest }', () => {
+    const modal = useModal()
+    const payload = { component: 'ModalLogin' }
+    const spy = jest.fn()
+    modal.listen(spy)
+
+    modal.close({ ...payload })
+
+    expect(spy).toBeCalledTimes(1)
+    expect(spy).toBeCalledWith({
+      status: false,
+      ...payload
+    })
+  })
+})
