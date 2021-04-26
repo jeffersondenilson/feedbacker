c1962a5f2ec723e2349f86bda4fa2a16e1c8e8de
Jefferson Denilson <jefersond121212@gmail.com>
2021-04-26 19:35:18 -0300

    testes de auth service e snapshots

--- dashboard/src/services/auth.spec.js
+++ dashboard/src/services/auth.spec.js
@@ -31,6 +31,45 @@ describe('AuthService', () => {
       return Promise.resolve({ data: user })
     })
 
-    // expect
+    const response = await AuthService(mockAxios).register(user)
+
+    expect(response.data).toHaveProperty('name')
+    expect(response.data).toHaveProperty('password')
+    expect(response.data).toHaveProperty('email')
+    expect(response).toMatchSnapshot()
+  })
+
+  it('should return errors on login response', async () => {
+    const errors = { status: 404, statusText: 'Not Found' }
+    mockAxios.post.mockImplementationOnce(() => {
+      return Promise.resolve({ request: errors })
+    })
+
+    const response = await AuthService(mockAxios)
+      .login({ email: 'me@me.com', password: '123' })
+
+    expect(response.data).toBe(undefined)
+    expect(response.errors).toHaveProperty('status')
+    expect(response.errors).toHaveProperty('statusText')
+    expect(response).toMatchSnapshot()
+  })
+
+  it('should return errors on register response', async () => {
+    const user = {
+      name: 'Me',
+      password: '123',
+      email: 'me@me.com'
+    }
+    const errors = { status: 400, statusText: 'Bad request' }
+    mockAxios.post.mockImplementationOnce(() => {
+      return Promise.resolve({ request: errors })
+    })
+
+    const response = await AuthService(mockAxios).register(user)
+
+    expect(response.data).toBe(undefined)
+    expect(response.errors).toHaveProperty('status')
+    expect(response.errors).toHaveProperty('statusText')
+    expect(response).toMatchSnapshot()
   })
 })
