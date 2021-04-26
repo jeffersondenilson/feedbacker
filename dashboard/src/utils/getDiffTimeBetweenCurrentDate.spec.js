a920b85f967b8dd49b9bb5738beb4356b2368f39
Jefferson Denilson <jefersond121212@gmail.com>
2021-04-26 07:36:42 -0300

    mais casos de teste

--- dashboard/src/utils/getDiffTimeBetweenCurrentDate.spec.js
+++ dashboard/src/utils/getDiffTimeBetweenCurrentDate.spec.js
@@ -1,7 +1,8 @@
 import { getDiffTimeBetweenCurrentDate } from './getDiffTimeBetweenCurrentDate'
 
 const dayInMilliseconds = 86400000
-const now = new Date('2021-04-25T18:18:30.000Z')
+const minutesInMilliseconds = 3600000
+const now = new Date('2021-04-25T18:30:30.000Z')
 
 describe('getDiffTimeBetweenCurrentDate utils', () => {
   it('should return same value for null, undefined, false or true', () => {
@@ -19,7 +20,7 @@ describe('getDiffTimeBetweenCurrentDate utils', () => {
     expect(received).toBe(invalidDate)
   })
 
-  it('should give message with one day ago', () => {
+  it('should return one day ago', () => {
     const yesterday = now.getTime() - dayInMilliseconds
     
     const received = getDiffTimeBetweenCurrentDate(yesterday, now)
@@ -27,7 +28,7 @@ describe('getDiffTimeBetweenCurrentDate utils', () => {
     expect(received).toBe('1 dia atrás')
   })
 
-  it('should give message with two days ago', () => {
+  it('should return two days ago', () => {
     const beforeYesterday = now.getTime() - dayInMilliseconds * 2
     
     const received = getDiffTimeBetweenCurrentDate(beforeYesterday, now)
@@ -35,7 +36,7 @@ describe('getDiffTimeBetweenCurrentDate utils', () => {
     expect(received).toBe('2 dias atrás')
   })
 
-  it('should give message with one second ago', () => {
+  it('should return one second ago', () => {
     const oneSec = now.getTime() - 1 * 1000
     
     const received = getDiffTimeBetweenCurrentDate(oneSec, now)
@@ -43,11 +44,75 @@ describe('getDiffTimeBetweenCurrentDate utils', () => {
     expect(received).toBe('1 segundo atrás')
   })
 
-  it('should give message with fifteen seconds ago', () => {
+  it('should return fifteen seconds ago', () => {
     const fifteenSec = now.getTime() - 15 * 1000
     
     const received = getDiffTimeBetweenCurrentDate(fifteenSec, now)
     
     expect(received).toBe('15 segundos atrás')
+  })
+
+  it('should return one minute ago', () => {
+    const oneMin = now.getTime() - 1 * 60 * 1000
+    
+    const received = getDiffTimeBetweenCurrentDate(oneMin, now)
+    
+    expect(received).toBe('1 minuto atrás')
+  })
+
+  it('should return twenty three minutes ago', () => {
+    const twentyThreeMin = now.getTime() - 23 * 60 * 1000
+    
+    const received = getDiffTimeBetweenCurrentDate(twentyThreeMin, now)
+    
+    expect(received).toBe('23 minutos atrás')
+  })
+
+  it('should return one hour ago', () => {
+    const oneHour = now.getTime() - 1 * minutesInMilliseconds
+    
+    const received = getDiffTimeBetweenCurrentDate(oneHour, now)
+    
+    expect(received).toBe('1 hora atrás')
+  })
+
+  it('should return six hours ago', () => {
+    const sixHour = now.getTime() - 6 * minutesInMilliseconds
+    
+    const received = getDiffTimeBetweenCurrentDate(sixHour, now)
+    
+    expect(received).toBe('6 horas atrás')
+  })
+
+  it('should round second to minute when turning back minute', () => {
+    const prevMinute = now.getTime() - 31 * 1000
+    
+    const received = getDiffTimeBetweenCurrentDate(prevMinute, now)
+    
+    expect(received).toBe('1 minuto atrás')
+  })
+
+  it('should round minute to hour when turning back hour', () => {
+    const prevHour = now.getTime() - 31 * 60 * 1000
+    
+    const received = getDiffTimeBetweenCurrentDate(prevHour, now)
+    
+    expect(received).toBe('1 hora atrás')
+  })
+
+  it('should round hour to day when turning back day', () => {
+    const prevDay = now.getTime() - 19 * minutesInMilliseconds
+    
+    const received = getDiffTimeBetweenCurrentDate(prevDay, now)
+    
+    expect(received).toBe('1 dia atrás')
+  })
+  // TODO: passar teste
+  it.skip('should return fifteen hours ago', () => {
+    const fifteenHours = now.getTime() - 15 * minutesInMilliseconds
+    
+    const received = getDiffTimeBetweenCurrentDate(fifteenHours, now)
+    
+    expect(received).toBe('15 horas atrás')
   })
 })
