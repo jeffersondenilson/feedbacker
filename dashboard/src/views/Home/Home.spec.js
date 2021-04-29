d96cff0806a3960784f0154e90393658f87a4cb4
Jefferson Denilson <jefersond121212@gmail.com>
2021-04-29 16:31:41 -0300

    testes p/ metodos de Home

--- dashboard/src/views/Home/Home.spec.js
+++ dashboard/src/views/Home/Home.spec.js
@@ -2,10 +2,31 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { shallowMount } from '@vue/test-utils'
 import Home from '.'
 import { routes } from '@/router'
+import bus from '@/utils/bus'
+import useModal from '@/hooks/useModal'
 
 const router = createRouter({
   history: createWebHistory('/'),
   routes
+})
+
+const EVENT_NAME = 'modal:toggle'
+const mockUseModal = {
+  listen (fn) {
+    bus.on(EVENT_NAME, fn)
+  },
+  open (payload = {}) {
+    bus.emit(EVENT_NAME, { ...payload, status: true })
+  }
+}
+jest.mock('@/hooks/useModal', () => {
+  // executa quando o arquivo é importado
+  return () => {
+    return {
+      listen: mockUseModal.listen,
+      open: mockUseModal.open
+    }
+  }
 })
 
 describe('<Home />', () => {
@@ -19,5 +40,47 @@ describe('<Home />', () => {
     })
     
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should call handleLogin with component = ModalLogin', async () => {
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(Home, {
+      global: {
+        plugins: [router]
+      }
+    })
+    const spy = jest.fn()
+    const modal = useModal()
+    modal.listen(spy)
+
+    wrapper.vm.handleLogin()
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith({
+      component: 'ModalLogin',
+      status: true
+    })
+  })
+
+  it('should call handleCreateAccount with component = ModalCreateAccount', async () => {
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(Home, {
+      global: {
+        plugins: [router]
+      }
+    })
+    const spy = jest.fn()
+    const modal = useModal()
+    modal.listen(spy)
+
+    wrapper.vm.handleCreateAccount()
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith({
+      component: 'ModalCreateAccount',
+      status: true
+    })
   })
 })
