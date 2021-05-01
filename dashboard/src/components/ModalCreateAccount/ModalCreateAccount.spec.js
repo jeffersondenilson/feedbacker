93827b72b06823c86d28744b3e6e2533d469610e
Jefferson Denilson <jefersond121212@gmail.com>
2021-05-01 16:06:30 -0300

    testes p/ ModalCreateAccount

---
+++ dashboard/src/components/ModalCreateAccount/ModalCreateAccount.spec.js
@@ -0,0 +1,289 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from '@/router'
+import Toast, { POSITION } from 'vue-toastification'
+import { shallowMount } from '@vue/test-utils'
+import ModalCreateAccount from '.'
+import { useField } from 'vee-validate'
+import bus from '@/utils/bus'
+import useModal from '@/hooks/useModal'
+
+jest.mock('vee-validate', () => {
+  return {
+    useField: jest.fn(() => {
+      return { value: '', errorMessage: '' }
+    })
+  }
+})
+
+const EVENT_NAME = 'modal:toggle'
+const mockUseModal = {
+  listen (fn) {
+    bus.on(EVENT_NAME, fn)
+  },
+  close (payload = {}) {
+    bus.emit(EVENT_NAME, { ...payload, status: false })
+  }
+}
+jest.mock('@/hooks/useModal', () => {
+  // executa quando o arquivo é importado
+  return () => {
+    return {
+      listen: mockUseModal.listen,
+      close: mockUseModal.close
+    }
+  }
+})
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+describe('<ModalCreateAccount />', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should render modal-create-account correctly', async () => {
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should set correct value in name input', async () => {
+    const name = 'name'
+    useField.mockReturnValue({ value: name, errorMessage: '' })
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+
+    const nameInput = wrapper.find('[data-test=name-input]')
+    const nameErrorMessage = wrapper.find('[data-test=name-error-message]')
+    await nameInput.setValue(name)
+
+    expect(nameInput.element.value).toBe(name)
+    expect(nameErrorMessage.exists()).toBe(false)
+  })
+
+  it('should show error with invalid name', async () => {
+    const name = ''
+    const errorMessage = '*Este campo é obrigatório'
+    useField.mockImplementation((field, validator) => {
+      if(field === 'name') {
+        return { value: name, errorMessage }
+      }
+      return { value: '', errorMessage: '' }
+    })
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+
+    const nameInput = wrapper.find('[data-test=name-input]')
+    const nameErrorMessage = wrapper.find('[data-test=name-error-message]')
+    await nameInput.setValue(name)
+
+    expect(nameInput.element.value).toBe(name)
+    expect(nameInput.classes('border-brand-danger')).toBe(true)
+    expect(nameErrorMessage.exists()).toBe(true)
+    expect(nameErrorMessage.text()).toBe(errorMessage)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should set correct value in email input', async () => {
+    const email = 'valid@email.com'
+    useField.mockReturnValue({ value: email, errorMessage: '' })
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+
+    const emailInput = wrapper.find('[data-test=email-input]')
+    const emailErrorMessage = wrapper.find('[data-test=email-error-message]')
+    await emailInput.setValue(email)
+
+    expect(emailInput.element.value).toBe(email)
+    expect(emailErrorMessage.exists()).toBe(false)
+  })
+
+  it('should show error with invalid email', async () => {
+    const email = 'invalid.email'
+    const errorMessage = '*Este campo precisa ser um e-mail válido'
+    useField.mockImplementation((field, validator) => {
+      if(field === 'email') {
+        return { value: email, errorMessage }
+      }
+      return { value: '', errorMessage: '' }
+    })
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+
+    const emailInput = wrapper.find('[data-test=email-input]')
+    const emailErrorMessage = wrapper.find('[data-test=email-error-message]')
+    await emailInput.setValue(email)
+
+    expect(emailInput.element.value).toBe(email)
+    expect(emailInput.classes('border-brand-danger')).toBe(true)
+    expect(emailErrorMessage.exists()).toBe(true)
+    expect(emailErrorMessage.text()).toBe(errorMessage)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should set correct value in password input', async () => {
+    const password = '123'
+    useField.mockReturnValue({ value: password, errorMessage: '' })
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+
+    const passwordInput = wrapper.find('[data-test=password-input]')
+    const passwordErrorMessage = wrapper.find('[data-test=password-error-message]')
+    await passwordInput.setValue(password)
+
+    expect(passwordInput.element.value).toBe(password)
+    expect(passwordErrorMessage.exists()).toBe(false)
+  })
+
+  it('should show error with invalid password', async () => {
+    const password = '12'
+    const errorMessage = '*No mínimo 3 caracteres'
+    useField.mockImplementation((field, validator) => {
+      if(field === 'password') {
+        return { value: password, errorMessage }
+      }
+      return { value: '', errorMessage: '' }
+    })
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+
+    const passwordInput = wrapper.find('[data-test=password-input]')
+    const passwordErrorMessage = wrapper.find('[data-test=password-error-message]')
+    await passwordInput.setValue(password)
+
+    expect(passwordInput.element.value).toBe(password)
+    expect(passwordInput.classes('border-brand-danger')).toBe(true)
+    expect(passwordErrorMessage.exists()).toBe(true)
+    expect(passwordErrorMessage.text()).toBe(errorMessage)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should trigger form submition', async () => {
+    const spy = jest.fn()
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ],
+        mocks: {
+          handleSubmit: spy
+        }
+      }
+    })
+    
+    const submitButton = wrapper.find('[data-test=create-account-submit-button]')
+    await submitButton.trigger('submit')
+
+    expect(wrapper.emitted()).toHaveProperty('submit')
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should show loading icon when button is clicked', async () => {
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+    
+    const submitButton = wrapper.find('[data-test=create-account-submit-button]')
+    expect(submitButton.text()).toBe('Entrar')
+    await submitButton.trigger('submit')
+
+    expect(submitButton.classes('opacity-50')).toBe(true)
+    expect(submitButton.attributes()).toHaveProperty('disabled')
+
+    const loadingIcon = wrapper.find('[data-test=submit-loading-icon]')
+    expect(loadingIcon.exists()).toBe(true)
+    expect(submitButton.html()).toMatchSnapshot()
+  })
+
+  it('should emit modal close when click close button', async () => {
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(ModalCreateAccount, {
+      global: {
+        plugins: [
+          router,
+          [Toast, { position: POSITION.BOTTOM_RIGHT }]
+        ]
+      }
+    })
+    const spy = jest.fn()
+    const modal = useModal()
+    modal.listen(spy)
+
+    const closeButton = wrapper.find('[data-test=close-button]')
+    closeButton.trigger('click')
+
+    expect(wrapper.emitted()).toHaveProperty('click')
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+})
--- dashboard/src/components/ModalCreateAccount/index.vue
+++ dashboard/src/components/ModalCreateAccount/index.vue
@@ -7,13 +7,14 @@
     <button
       @click="close"
       class="text-4xl text-gray-600 focus:outline-none"
+      data-test="close-button"
     >
       &times;
     </button>
   </div>
 
   <div class="mt-16">
-    <form @submit.prevent="handleSubmit">
+    <form @submit.prevent="handleSubmit" data-test="create-account-form">
       <label class="block">
         <span class="text-lg font-medium text-gray-800">Nome</span>
         <input
@@ -22,10 +23,12 @@
           :class="{ 'border-brand-danger': !!state.name.errorMessage }"
           class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
           placeholder="Jane Doe"
+          data-test="name-input"
         />
         <span
           v-if="!!state.name.errorMessage"
           class="block font-medium text-brand-danger"
+          data-test="name-error-message"
         >
           {{ state.name.errorMessage }}
         </span>
@@ -39,10 +42,12 @@
           :class="{ 'border-brand-danger': !!state.email.errorMessage }"
           class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
           placeholder="jane.doe@gmail.com"
+          data-test="email-input"
         />
         <span
           v-if="!!state.email.errorMessage"
           class="block font-medium text-brand-danger"
+          data-test="email-error-message"
         >
           {{ state.email.errorMessage }}
         </span>
@@ -56,10 +61,12 @@
           :class="{ 'border-brand-danger': !!state.password.errorMessage }"
           class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
           placeholder="******"
+          data-test="password-input"
         />
         <span
           v-if="!!state.password.errorMessage"
           class="block font-medium text-brand-danger"
+          data-test="password-error-message"
         >
           {{ state.password.errorMessage }}
         </span>
@@ -70,8 +77,14 @@
         type="submit"
         :class="{ 'opacity-50': state.isLoading }"
         class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
+        data-test="create-account-submit-button"
       >
-        <icon v-if="state.isLoading" name="loading" class="animate-spin" />
+        <icon
+          v-if="state.isLoading"
+          name="loading"
+          class="animate-spin"
+          data-test="submit-loading-icon"
+        />
         <span v-else>Entrar</span>
       </button>
     </form>
